696ade698743f7bf896295c6e1d04358e3b21c37
AshveerB <ashveerbhayroo@gmail.com>
2022-03-07 12:57:39 -0800

    add email route

---
+++ client/src/components/Email.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Email(props) {
+    return (
+        <div>
+            Email
+        </div>
+    );
+}
+
+export default Email;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const cors = require('cors');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+require('dotenv').config();
 
 const io = require('socket.io')(server, {
 	cors: {
@@ -10,8 +14,42 @@ const io = require('socket.io')(server, {
 });
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
+
+let to;
+let subject;
+let body;
+
+app.post('/sendmail', (req, res) => {
+	to = req.body.to;
+	subject = req.body.subject;
+	body = req.body.body;
+	let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'ashveertest@gmail.com',
+			pass: process.env.EMAIL_PASS,
+		},
+	});
+
+	let mailOptions = {
+		from: 'ashveertest@gmail.com',
+		to: to,
+		subject: subject,
+		text: body,
+	};
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+		}
+	});
+});
 
 app.get('/', (req, res) => {
 	res.send('Server is running');
@@ -32,5 +70,7 @@ io.on('connection', (socket) => {
 		io.to(data.to).emit('callAccepted', data.signal);
 	});
 });
+
+
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
